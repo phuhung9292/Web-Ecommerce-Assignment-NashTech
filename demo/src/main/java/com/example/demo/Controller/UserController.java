7f037584018ce260d002bbf62f8572086cb2173e
phuhung9292 <phuhungx1810@gmail.com>
2022-11-02 01:18:35 +0700

    update API add to cart

--- demo/src/main/java/com/example/demo/Controller/UserController.java
+++ demo/src/main/java/com/example/demo/Controller/UserController.java
@@ -16,6 +16,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
+
+@CrossOrigin(origins = "http://localhost:3000")
+
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
--- demo/src/main/java/com/example/demo/Dto/CategoryDto.java
+++ demo/src/main/java/com/example/demo/Dto/CategoryDto.java
@@ -3,7 +3,7 @@ package com.example.demo.Dto;
 import lombok.Data;
 
 @Data
-public class CategoryDto {
+public class  CategoryDto {
     private int id;
     private String typeCategory;
 }
